7748c5b3f82bbfa73cc2fc4ae1911fe32bc0bbb7
luben <lubenfrank48@gmail.com>
2019-03-27 15:09:57 +0530

    node.js tennis player

---
+++ projects/node.js/addTenniPlayers/app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// Include express in our application
+var express = require("express");
+
+// Include body-parser in our application
+var bodyParser = require("body-parser");
+
+// Create the express "app"
+var app = express();
+
+// Tell ExpressJS that we are using Handlebars for the project
+app.set("view engine", "hbs");
+
+// Tell ExpressJS to use body-parser to receive form data.
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Global players variable to hold data
+var playersData = [];
+
+app.get("/players", function(request, response) {
+  var data = {
+    players: playersData,
+    userName: "Luben"
+  };
+  response.render("players.hbs", data);
+});
+
+app.post("/players", function(request, response) {
+  var playerName = request.body.playerName;
+  var playerLocation = request.body.playerLocation;
+  var obj = {};
+  obj.name = playerName;
+  obj.location = playerLocation;
+  playersData.push(obj);
+
+  // Once data is added, redirect to the GET page.
+  response.redirect("/players");
+});
+
+app.listen(3000);
